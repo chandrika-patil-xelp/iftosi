e8b9b016930869702f4fc917bff75cc086cad4ca
rishi <rishi@b2388e85-c94f-4185-8f27-f3c550f21351>
2015-12-09 17:10:53 +0000

    Bug fixes.

--- tools/js/adminNav.js
+++ tools/js/adminNav.js
@@ -1,17 +1,22 @@
 $(document).ready(function () {
     var uid = customStorage.readFromStorage('userid');
     var is_vendor = customStorage.readFromStorage('is_vendor');
-    if(is_vendor == 2)
-    {
-        $('#productstab').bind('click',function(){
-        window.location.href=DOMAIN+'index.php?case=product_list&uid='+uid;
-        });
-        $('#vendorlist').bind('click',function(){
-        window.location.href=DOMAIN+'index.php?case=vendorList&uid='+uid;
-        });
-    }
-    else
-    {
-        window.location.href = DOMAIN;
-    }
+	var isLoggedIn = customStorage.readFromStorage('isLoggedIn');
+
+	if(isLoggedIn !== undefined && isLoggedIn !== null && isLoggedIn !== '' || typeof isLoggedIn !== 'undefined' && (isLoggedIn == 'true' || isLoggedIn == true))
+	{
+		if(is_vendor == 2)
+		{
+			$('#productstab').bind('click',function(){
+			window.location.href=DOMAIN+'index.php?case=product_list&uid='+uid;
+			});
+			$('#vendorlist').bind('click',function(){
+			window.location.href=DOMAIN+'index.php?case=vendorList&uid='+uid;
+			});
+		}
+		else
+		{
+			window.location.href = DOMAIN;
+		}
+	}
 });
