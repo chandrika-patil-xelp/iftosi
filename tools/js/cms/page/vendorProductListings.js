d53b65d4d521c18b981764526f8648e331234750
shubham <shubham@b2388e85-c94f-4185-8f27-f3c550f21351>
2015-12-28 11:11:36 +0000

    Edit btn

--- tools/js/cms/page/vendorProductListings.js
+++ tools/js/cms/page/vendorProductListings.js
@@ -277,7 +277,7 @@ function generateDiamondList(obj) {
     if(obj['active_flag'] == 3){
         cl='outofstock';
     }
-    str += '<select id="isStock'+ obj['id'] +'" onchange="inStock(' + obj['id'] + ',this.value);" value='+obj['active_flag']+' class="txtInput divCon poR fmOpenR font14 c666 txtCenter divCon" style="background-image: url(tools/img/common/down_arrow.svg);background-repeat: no-repeat;background-position: right center;line-height:15px;width:50%;margin-top:1%">';
+    str += '<select id="isStock'+ obj['id'] +'" onchange="inStock(' + obj['id'] + ',this.value);" value='+obj['active_flag']+' class="txtSelect fmOpenR">';
     str += '<option '+((obj['active_flag']) == 1 ? "selected" : "")+' class="arrow txtCenter" value="1">In Stock</option>';
     str += '<option '+((obj['active_flag']) == 3 ? "selected" : "")+' class="arrow txtCenter" value="3">Out Of Stock</option>';
     str += '<option '+((obj['active_flag']) == 4 ? "selected" : "")+' class="arrow txtCenter" value="4">Sold</option>';
@@ -439,7 +439,7 @@ function generateJewellList(obj) {
         if(obj['active_flag']==3){
         cl='outofstock';
         }
-        str += '<select id="isStock'+ obj['id'] +'" onchange="inStock(' + obj['id'] + ',this.value);" value='+obj['active_flag']+' class="txtInput divCon poR fmOpenR font14 c666 txtCenter divCon" style="background-image: url(tools/img/common/down_arrow.svg);background-repeat: no-repeat;background-position: right center;line-height:15px;width:50%;margin-top:1%">';
+        str += '<select id="isStock'+ obj['id'] +'" onchange="inStock(' + obj['id'] + ',this.value);" value='+obj['active_flag']+' class="txtSelect fmOpenR">';
         str += '<option value="1" '+((obj['active_flag']) == 1 ? "selected" : "")+'>In Stock</option>';
         str += '<option value="3" '+((obj['active_flag']) == 3 ? "selected" : "")+'>Out Of Stock</option>';
         str += '</select>';
@@ -582,7 +582,7 @@ function generatBullionsList(obj) {
     if(obj['active_flag']==3){
     cl='outofstock';
     }
-    str += '<select id="isStock'+ obj['id'] +'" onchange="inStock(' + obj['id'] + ',this.value);" value='+obj['active_flag']+' class="txtInput divCon poR fmOpenR font14 c666 txtCenter divCon" style="background-image: url(tools/img/common/down_arrow.svg);background-repeat: no-repeat;background-position: right center;line-height:15px;width:50%;margin-top:1%">';
+    str += '<select id="isStock'+ obj['id'] +'" onchange="inStock(' + obj['id'] + ',this.value);" value='+obj['active_flag']+' class="txtSelect fmOpenR">';
     str += '<option value="1" '+((obj['active_flag']) == 1 ? "selected" : "")+'>In Stock</option>';
     str += '<option value="3" '+((obj['active_flag']) == 3 ? "selected" : "")+'>Out Of Stock</option>';
     str += '</select>';
