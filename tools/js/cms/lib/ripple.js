adb3e79b1e2cb421528ce9298a68e3ffd2585f08
silentmajority <silentmajority@b2388e85-c94f-4185-8f27-f3c550f21351>
2016-02-24 07:19:47 +0000

    removed ripple link for frontend and cms

--- tools/js/cms/lib/ripple.js
+++ tools/js/cms/lib/ripple.js
@@ -1,26 +1,3 @@
 $(function() {
-    var useragent = navigator.userAgent;
-    //if(useragent.toLowerCase().indexOf('firefox') === -1)
-    if(useragent.toLowerCase().indexOf('firefox') === false)
-    {
-        var ink, d, x, y;
-        $(".ripplelink").bind('mousedown ontouchstart tap', function(e) {
-            if ($(this).find(".ink").length === 0) {
-                $(this).prepend("<span class='ink'></span>");
-            }
-
-            ink = $(this).find(".ink");
-            ink.removeClass("animate");
-
-            if (!ink.height() && !ink.width()) {
-                d = Math.max($(this).outerWidth(), $(this).outerHeight());
-                ink.css({height: d, width: d});
-            }
-
-            x = e.pageX - $(this).offset().left - ink.width() / 2;
-            y = e.pageY - $(this).offset().top - ink.height() / 2;
-
-            ink.css({top: y + 'px', left: x + 'px'}).addClass("animate");
-        });
-    }
+    
 });
--- tools/js/lib/ripple.js
+++ tools/js/lib/ripple.js
@@ -1,26 +1,3 @@
 $(function() {
-	var uagent = navigator.userAgent.toLowerCase();
-	//if(uagent.indexOf('firefox') === -1)
-	if(false)
-	{
-		var ink, d, x, y;
-		$(".ripplelink").bind('mousedown ontouchstart tap', function(e) {
-			if ($(this).find(".ink").length === 0) {
-				$(this).prepend("<span class='ink'></span>");
-			}
-
-            ink = $(this).find(".ink");
-            ink.removeClass("animate");
-
-            if (!ink.height() && !ink.width()) {
-                d = Math.max($(this).outerWidth(), $(this).outerHeight());
-                ink.css({height: d, width: d});
-            }
-
-            x = e.pageX - $(this).offset().left - ink.width() / 2;
-            y = e.pageY - $(this).offset().top - ink.height() / 2;
-
-            ink.css({top: y + 'px', left: x + 'px'}).addClass("animate");
-        });
-    }
+	
 });
